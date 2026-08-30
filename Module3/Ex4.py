c736053d19f5ea374108257db32cfcab8aaedb1b
abdullra <abdullra@metropolia.fi>
2026-08-30 20:34:04 +0400

    Add Module 3 exercises

---
+++ Module3/Ex4.py
@@ -0,0 +1,10 @@
+#n = number 
+n1 = int(input("Enter the first number: "))
+n2 = int(input("Enter the second number: "))
+n3 = int(input("Enter the third number: "))
+sum = n1 + n2 + n3
+product = n1 * n2 * n3
+average = sum / 3
+print(f"Sum: {sum}")
+print(f"Product: {product}")
+print(f"Average: {average}")
---
+++ Module3/Ex5.py
@@ -0,0 +1,9 @@
+talents = float(input("Enter talents: "))
+pounds = float(input("Enter pounds: "))
+lots = float(input("Enter lots: "))
+total_lots = (talents * 20 * 32) + (pounds * 32) + lots
+total_g = total_lots * 13.3
+kg = int(total_g // 1000)
+g = total_g % 1000
+print("The weight in modern units:")
+print(f"kilogram: {kg}, grams: {g}")
